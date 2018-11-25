cac110bddb4092461daf37280d6887e9e035e1d3
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-11-24 16:52:09 -0800

    readJson

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function Card(obj) {
+  this.url = obj.image_url;
+  this.title = obj.title;
+  this.description = obj.description;
+  this.keyword = obj.keyword;
+  this.horns = obj.horns;
+
+  allCards.push(this);
+}
+
+const allCards = [];
+
+Card.prototype.render = function() {
+  console.log(this);
+};
+
+function readJson() {
+  $.get('data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(o => new Card(o));
+    })
+    .then(() => {
+      allCards.forEach(c => c.render());
+    });
+}
+
+function init() {
+  // get data
+  // render
+}
+
+$(() => readJson());
